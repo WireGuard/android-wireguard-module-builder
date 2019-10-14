cf16c7858c51c1802fb82beca70d559f58476419
Nathan Chancellor <natechancellor@gmail.com>
2019-10-14 14:07:20 -0700

    kernels: add marlin manifest and build commands
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernels/marlin/do.bash
+++ kernels/marlin/do.bash
@@ -2,3 +2,6 @@
 set -ex
 BASE="$(readlink -f "$(dirname "$(readlink -f "$0")")")"
 patch -d WireGuard -p1 < "$BASE/tvec_base_deferrable-hack.patch"
+echo 'EXT_MODULES="${EXT_MODULES} wireguard"' >> private/msm-google/build.config
+./build/build.sh
+readlink -f out/android-msm-marlin-3.18/dist/wireguard.ko >&7
