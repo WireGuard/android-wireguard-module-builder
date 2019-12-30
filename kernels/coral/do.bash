8391037f86b95a4986b23b61f6f32efa760dd8a6
Nathan Chancellor <natechancellor@gmail.com>
2019-12-30 16:27:29 -0700

    kernels/coral: apply a patch to fix glibc 2.29+
    Link: https://lore.kernel.org/lkml/20190422210041.GA21711@archlinux-i9/
    Link: https://git.kernel.org/stable/c/760f8522ce08a24abac3208290f93fe3fffc0d6c
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernels/coral/do.bash
+++ kernels/coral/do.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+BASE="$(readlink -f "$(dirname "$(readlink -f "$0")")")"
+patch -d private/msm-google -p1 < "$BASE/selinux.patch"
 echo 'EXT_MODULES="${EXT_MODULES} wireguard"' >> private/msm-google/build.config.common
 echo 'ccflags-y += -Wno-unused-variable' >> wireguard-linux-compat/src/Kbuild
 ./build/build.sh
