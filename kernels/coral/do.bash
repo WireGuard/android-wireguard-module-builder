e76d74e745f57176a675512bf8c39362cc535182
Jason A. Donenfeld <Jason@zx2c4.com>
2020-02-05 17:30:11 +0100

    kernels: strip debug symbols from older-style kernels
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- kernels/coral/do.bash
+++ kernels/coral/do.bash
@@ -5,4 +5,5 @@ patch -d private/msm-google -p1 < "$BASE/selinux.patch"
 echo 'EXT_MODULES="${EXT_MODULES} wireguard"' >> private/msm-google/build.config.common
 echo 'ccflags-y += -Wno-unused-variable' >> wireguard-linux-compat/src/Kbuild
 ./build/build.sh
+./prebuilts-master/clang/host/linux-x86/clang-r346389c/bin/llvm-strip -strip-debug out/android-msm-floral-4.14/dist/wireguard.ko
 readlink -f out/android-msm-floral-4.14/dist/wireguard.ko >&7
--- kernels/ocn/do.bash
+++ kernels/ocn/do.bash
@@ -13,4 +13,5 @@ mkdir out
 make ARCH=arm64 CROSS_COMPILE="$PWD/../aarch64-linux-android-4.9/bin/aarch64-linux-android-" O=out htcperf_defconfig
 make ARCH=arm64 CROSS_COMPILE="$PWD/../aarch64-linux-android-4.9/bin/aarch64-linux-android-" O=out -j$(nproc)
 
+../aarch64-linux-android-4.9/bin/aarch64-linux-android-strip --strip-debug out/net/wireguard/wireguard.ko
 readlink -f out/net/wireguard/wireguard.ko >&7
