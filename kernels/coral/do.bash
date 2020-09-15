f9a2cc98a5acc5a9cd16289d33f6095af15b5563
Jason A. Donenfeld <Jason@zx2c4.com>
2020-09-15 14:06:16 +0200

    coral: use newer clang toolchain revision
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- kernels/coral/do.bash
+++ kernels/coral/do.bash
@@ -4,5 +4,5 @@ BASE="$(readlink -f "$(dirname "$(readlink -f "$0")")")"
 echo 'EXT_MODULES="${EXT_MODULES} wireguard"' >> private/msm-google/build.config.common
 echo 'ccflags-y += -Wno-unused-variable' >> wireguard-linux-compat/src/Kbuild
 ./build/build.sh
-./prebuilts-master/clang/host/linux-x86/clang-r346389c/bin/llvm-strip -strip-debug out/android-msm-floral-4.14/dist/wireguard.ko
+./prebuilts-master/clang/host/linux-x86/clang-r353983c/bin/llvm-strip -strip-debug out/android-msm-floral-4.14/dist/wireguard.ko
 readlink -f out/android-msm-floral-4.14/dist/wireguard.ko >&7
