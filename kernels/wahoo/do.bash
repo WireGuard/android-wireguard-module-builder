dce176b8a77ce7585b66391d32d4c4a2da73f3f6
Nathan Chancellor <natechancellor@gmail.com>
2020-09-14 23:02:41 -0700

    September update
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernels/wahoo/do.bash
+++ kernels/wahoo/do.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -ex
-BASE="$(readlink -f "$(dirname "$(readlink -f "$0")")")"
-patch -d private/msm-google -p1 < "$BASE/0001-BACKPORT-arm64-makefile-fix-build-of-.i-file-in-exte.patch"
 echo 'EXT_MODULES="${EXT_MODULES} wireguard"' >> private/msm-google/build.config
 ./build/build.sh
 readlink -f out/android-msm-wahoo-4.4/dist/wireguard.ko >&7
